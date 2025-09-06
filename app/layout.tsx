29ad4704e799f203e505c77d9a070c80d89f536b
fareedgul <fareedgul735@gmail.com>
2025-09-06 04:53:33 -0700

    fix/changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
-import Footer from "@/components/Footer"
-import Header from "@/components/Header"
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
-import "./styles/global.css"
+import "./styles/global.css";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Horror Layout",
+  icons: {
+    icon: "/anabelle.jpg",
+  },
+};
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-
   return (
     <html>
       <body>
@@ -13,7 +19,7 @@ const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
         <Footer />
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
